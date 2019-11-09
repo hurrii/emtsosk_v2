ea97f2ce9fe69b3422da015da3c60c38c1359c1a
hurrii <hurrii.ftw@gmail.com>
2019-11-09 17:40:09 +0300

    добавить preloader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 // import _ from 'lodash';
 require('src/main.styl');
 require('assets/favicon.ico');
+
+
+window.addEventListener('load', () => {
+
+	setTimeout(() => {
+		document.querySelector('.page').style.visibility = 'visible';
+		document.querySelector('.page').classList.add('page--active');
+		document.querySelector('.preloader').style.visibility = 'hidden';
+	}, 1000);
+});
