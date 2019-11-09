2efbe4b98398d250244b47e7c00f7d0a098b4779
hurrii <hurrii.ftw@gmail.com>
2019-11-09 22:05:05 +0300

    добавить интерактивный фон

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // import _ from 'lodash';
 require('src/main.styl');
 require('assets/favicon.ico');
+import CanvasNest from 'canvas-nest.js';
+
+const canvasConfig = {
+	color: '0, 185, 229',
+	pointColor: '0, 185, 229',
+	count: 150,
+	opacity: 1,
+	zIndex: -2
+};
+
+const cn = new CanvasNest(document.querySelector('.background-canvas'), canvasConfig);
+console.log(cn);
 
 
 window.addEventListener('load', () => {
